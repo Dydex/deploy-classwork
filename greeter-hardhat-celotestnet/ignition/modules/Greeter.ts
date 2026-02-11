6600cde22b2d41059cc57c41b374b03de5b00262
Dydex <supremeajala@gmail.com>
2026-02-11 17:49:51 +0100

    deployed CA

---
+++ greeter-hardhat-celotestnet/ignition/modules/Greeter.ts
@@ -0,0 +1,17 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://v2.hardhat.org/ignition
+
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+
+
+
+const GreeterModule = buildModule("LockModule", (m) => {
+  
+
+  const Greeter = m.contract("Greeter");
+
+  return { Greeter };
+});
+
+export default GreeterModule;
